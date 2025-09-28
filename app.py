3e09fefed76ee52e0404e91fce1a1eadf617ac8e
Sagarkanojiya <sager.kanojiya19@gmail.com>
2025-09-28 17:37:15 +0530

    updation

--- app.py
+++ app.py
@@ -215,6 +215,16 @@ if 'use_browser_rec' not in st.session_state:
 st.markdown(
     """
     <style>
+    .mic-wrap div.stButton {
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    width: 100% !important;
+    }
+    .mic-wrap .stButton>button {
+    margin: 0 auto !important;
+    display: flex !important;
+    }
     .main { background: linear-gradient(135deg, #0b0b0b, #141414); }
     #MainMenu, header, footer { visibility: hidden; }
     .main .block-container { padding-top: 0.6rem; }
@@ -245,6 +255,7 @@ st.markdown(
     .analysis-title { font-size: 1.6rem; font-weight: 750; margin: 0 0 .35rem; }
     .analysis-subtitle { color:#bdbdbd; font-size: 1rem; margin: 0 0 .8rem; }
 
+    
     .mic-area { width:100%; display:flex; justify-content:center; align-items:center; padding:.4rem 0 .1rem; margin: 0 auto; position: relative; min-height: 120px; }
     .mic-wrap { max-width: 980px; margin: 0 auto; display: grid; place-items: center; }
     .mic-area .stButton>button {
